d8a3cb1aaa20a44856cc462358b126792470003d
Ling Samuel <lingsamuelgrace@gmail.com>
2022-05-18 17:05:39 +0800

    e2e-test: add e2e tests and CRDs for ApisixConsumer v2
    Signed-off-by: Ling Samuel <lingsamuelgrace@gmail.com>

--- test/e2e/suite-annotations/authorization.go
+++ test/e2e/suite-annotations/authorization.go
@@ -16,27 +16,26 @@ package annotations
 
 import (
 	"fmt"
-	"net/http"
-	"time"
-
+	"github.com/apache/apisix-ingress-controller/test/e2e/scaffold"
 	"github.com/onsi/ginkgo"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/apache/apisix-ingress-controller/test/e2e/scaffold"
+	"net/http"
+	"time"
 )
 
 var _ = ginkgo.Describe("suite-annotations: authorization annotations", func() {
-	s := scaffold.NewDefaultScaffold()
+	suites := func(scaffoldFunc func() *scaffold.Scaffold) {
+		s := scaffoldFunc()
 
-	ginkgo.It("enable keyAuth in ingress networking/v1", func() {
-		err := s.ApisixConsumerKeyAuthCreated("foo", "bar")
-		assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
+		ginkgo.It("enable keyAuth in ingress networking/v1", func() {
+			err := s.ApisixConsumerKeyAuthCreated("foo", "bar")
+			assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
 
-		// Wait until the ApisixConsumer create event was delivered.
-		time.Sleep(6 * time.Second)
+			// Wait until the ApisixConsumer create event was delivered.
+			time.Sleep(6 * time.Second)
 
-		backendSvc, backendPort := s.DefaultHTTPBackend()
-		ing := fmt.Sprintf(`
+			backendSvc, backendPort := s.DefaultHTTPBackend()
+			ing := fmt.Sprintf(`
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
@@ -57,34 +56,34 @@ spec:
             port:
               number: %d
 `, backendSvc, backendPort[0])
-		err = s.CreateResourceFromString(ing)
-		assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
-		time.Sleep(5 * time.Second)
+			err = s.CreateResourceFromString(ing)
+			assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
+			time.Sleep(5 * time.Second)
 
-		msg401 := s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			Expect().
-			Status(http.StatusUnauthorized).
-			Body().
-			Raw()
-		assert.Contains(ginkgo.GinkgoT(), msg401, "Missing API key found in request")
+			msg401 := s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				Expect().
+				Status(http.StatusUnauthorized).
+				Body().
+				Raw()
+			assert.Contains(ginkgo.GinkgoT(), msg401, "Missing API key found in request")
 
-		_ = s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			WithHeader("apikey", "bar").
-			Expect().
-			Status(http.StatusOK)
-	})
+			_ = s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				WithHeader("apikey", "bar").
+				Expect().
+				Status(http.StatusOK)
+		})
 
-	ginkgo.It("enable keyAuth in ingress networking/v1beta1", func() {
-		err := s.ApisixConsumerKeyAuthCreated("foo", "bar")
-		assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
+		ginkgo.It("enable keyAuth in ingress networking/v1beta1", func() {
+			err := s.ApisixConsumerKeyAuthCreated("foo", "bar")
+			assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
 
-		// Wait until the ApisixConsumer create event was delivered.
-		time.Sleep(6 * time.Second)
+			// Wait until the ApisixConsumer create event was delivered.
+			time.Sleep(6 * time.Second)
 
-		backendSvc, backendPort := s.DefaultHTTPBackend()
-		ing := fmt.Sprintf(`
+			backendSvc, backendPort := s.DefaultHTTPBackend()
+			ing := fmt.Sprintf(`
 apiVersion: networking.k8s.io/v1beta1
 kind: Ingress
 metadata:
@@ -103,34 +102,34 @@ spec:
           serviceName: %s
           servicePort: %d
 `, backendSvc, backendPort[0])
-		err = s.CreateResourceFromString(ing)
-		assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
-		time.Sleep(5 * time.Second)
+			err = s.CreateResourceFromString(ing)
+			assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
+			time.Sleep(5 * time.Second)
 
-		msg401 := s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			Expect().
-			Status(http.StatusUnauthorized).
-			Body().
-			Raw()
-		assert.Contains(ginkgo.GinkgoT(), msg401, "Missing API key found in request")
+			msg401 := s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				Expect().
+				Status(http.StatusUnauthorized).
+				Body().
+				Raw()
+			assert.Contains(ginkgo.GinkgoT(), msg401, "Missing API key found in request")
 
-		_ = s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			WithHeader("apikey", "bar").
-			Expect().
-			Status(http.StatusOK)
-	})
+			_ = s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				WithHeader("apikey", "bar").
+				Expect().
+				Status(http.StatusOK)
+		})
 
-	ginkgo.It("enable keyAuth in ingress extensions/v1beta1", func() {
-		err := s.ApisixConsumerKeyAuthCreated("foo", "bar")
-		assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
+		ginkgo.It("enable keyAuth in ingress extensions/v1beta1", func() {
+			err := s.ApisixConsumerKeyAuthCreated("foo", "bar")
+			assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
 
-		// Wait until the ApisixConsumer create event was delivered.
-		time.Sleep(6 * time.Second)
+			// Wait until the ApisixConsumer create event was delivered.
+			time.Sleep(6 * time.Second)
 
-		backendSvc, backendPort := s.DefaultHTTPBackend()
-		ing := fmt.Sprintf(`
+			backendSvc, backendPort := s.DefaultHTTPBackend()
+			ing := fmt.Sprintf(`
 apiVersion: extensions/v1beta1
 kind: Ingress
 metadata:
@@ -149,34 +148,34 @@ spec:
           serviceName: %s
           servicePort: %d
 `, backendSvc, backendPort[0])
-		err = s.CreateResourceFromString(ing)
-		assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
-		time.Sleep(5 * time.Second)
+			err = s.CreateResourceFromString(ing)
+			assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
+			time.Sleep(5 * time.Second)
 
-		msg401 := s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			Expect().
-			Status(http.StatusUnauthorized).
-			Body().
-			Raw()
-		assert.Contains(ginkgo.GinkgoT(), msg401, "Missing API key found in request")
+			msg401 := s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				Expect().
+				Status(http.StatusUnauthorized).
+				Body().
+				Raw()
+			assert.Contains(ginkgo.GinkgoT(), msg401, "Missing API key found in request")
 
-		_ = s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			WithHeader("apikey", "bar").
-			Expect().
-			Status(http.StatusOK)
-	})
+			_ = s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				WithHeader("apikey", "bar").
+				Expect().
+				Status(http.StatusOK)
+		})
 
-	ginkgo.It("enable basicAuth in ingress networking/v1", func() {
-		err := s.ApisixConsumerBasicAuthCreated("jack1", "jack1-username", "jack1-password")
-		assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
+		ginkgo.It("enable basicAuth in ingress networking/v1", func() {
+			err := s.ApisixConsumerBasicAuthCreated("jack1", "jack1-username", "jack1-password")
+			assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
 
-		// Wait until the ApisixConsumer create event was delivered.
-		time.Sleep(6 * time.Second)
+			// Wait until the ApisixConsumer create event was delivered.
+			time.Sleep(6 * time.Second)
 
-		backendSvc, backendPort := s.DefaultHTTPBackend()
-		ing := fmt.Sprintf(`
+			backendSvc, backendPort := s.DefaultHTTPBackend()
+			ing := fmt.Sprintf(`
 apiVersion: networking.k8s.io/v1
 kind: Ingress
 metadata:
@@ -197,34 +196,34 @@ spec:
             port:
               number: %d
 `, backendSvc, backendPort[0])
-		err = s.CreateResourceFromString(ing)
-		assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
-		time.Sleep(5 * time.Second)
+			err = s.CreateResourceFromString(ing)
+			assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
+			time.Sleep(5 * time.Second)
 
-		msg401 := s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			Expect().
-			Status(http.StatusUnauthorized).
-			Body().
-			Raw()
-		assert.Contains(ginkgo.GinkgoT(), msg401, "Missing authorization in request")
+			msg401 := s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				Expect().
+				Status(http.StatusUnauthorized).
+				Body().
+				Raw()
+			assert.Contains(ginkgo.GinkgoT(), msg401, "Missing authorization in request")
 
-		_ = s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			WithHeader("Authorization", "Basic amFjazEtdXNlcm5hbWU6amFjazEtcGFzc3dvcmQ=").
-			Expect().
-			Status(http.StatusOK)
-	})
+			_ = s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				WithHeader("Authorization", "Basic amFjazEtdXNlcm5hbWU6amFjazEtcGFzc3dvcmQ=").
+				Expect().
+				Status(http.StatusOK)
+		})
 
-	ginkgo.It("enable basicAuth in ingress networking/v1beta1", func() {
-		err := s.ApisixConsumerBasicAuthCreated("jack1", "jack1-username", "jack1-password")
-		assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
+		ginkgo.It("enable basicAuth in ingress networking/v1beta1", func() {
+			err := s.ApisixConsumerBasicAuthCreated("jack1", "jack1-username", "jack1-password")
+			assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
 
-		// Wait until the ApisixConsumer create event was delivered.
-		time.Sleep(6 * time.Second)
+			// Wait until the ApisixConsumer create event was delivered.
+			time.Sleep(6 * time.Second)
 
-		backendSvc, backendPort := s.DefaultHTTPBackend()
-		ing := fmt.Sprintf(`
+			backendSvc, backendPort := s.DefaultHTTPBackend()
+			ing := fmt.Sprintf(`
 apiVersion: networking.k8s.io/v1beta1
 kind: Ingress
 metadata:
@@ -243,34 +242,34 @@ spec:
           serviceName: %s
           servicePort: %d
 `, backendSvc, backendPort[0])
-		err = s.CreateResourceFromString(ing)
-		assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
-		time.Sleep(5 * time.Second)
+			err = s.CreateResourceFromString(ing)
+			assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
+			time.Sleep(5 * time.Second)
 
-		msg401 := s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			Expect().
-			Status(http.StatusUnauthorized).
-			Body().
-			Raw()
-		assert.Contains(ginkgo.GinkgoT(), msg401, "Missing authorization in request")
+			msg401 := s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				Expect().
+				Status(http.StatusUnauthorized).
+				Body().
+				Raw()
+			assert.Contains(ginkgo.GinkgoT(), msg401, "Missing authorization in request")
 
-		_ = s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			WithHeader("Authorization", "Basic amFjazEtdXNlcm5hbWU6amFjazEtcGFzc3dvcmQ=").
-			Expect().
-			Status(http.StatusOK)
-	})
+			_ = s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				WithHeader("Authorization", "Basic amFjazEtdXNlcm5hbWU6amFjazEtcGFzc3dvcmQ=").
+				Expect().
+				Status(http.StatusOK)
+		})
 
-	ginkgo.It("enable basicAuth in ingress networking/v1beta1", func() {
-		err := s.ApisixConsumerBasicAuthCreated("jack1", "jack1-username", "jack1-password")
-		assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
+		ginkgo.It("enable basicAuth in ingress networking/v1beta1", func() {
+			err := s.ApisixConsumerBasicAuthCreated("jack1", "jack1-username", "jack1-password")
+			assert.Nil(ginkgo.GinkgoT(), err, "creating keyAuth ApisixConsumer")
 
-		// Wait until the ApisixConsumer create event was delivered.
-		time.Sleep(6 * time.Second)
+			// Wait until the ApisixConsumer create event was delivered.
+			time.Sleep(6 * time.Second)
 
-		backendSvc, backendPort := s.DefaultHTTPBackend()
-		ing := fmt.Sprintf(`
+			backendSvc, backendPort := s.DefaultHTTPBackend()
+			ing := fmt.Sprintf(`
 apiVersion: extensions/v1beta1
 kind: Ingress
 metadata:
@@ -289,22 +288,30 @@ spec:
           serviceName: %s
           servicePort: %d
 `, backendSvc, backendPort[0])
-		err = s.CreateResourceFromString(ing)
-		assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
-		time.Sleep(5 * time.Second)
+			err = s.CreateResourceFromString(ing)
+			assert.Nil(ginkgo.GinkgoT(), err, "creating ingress")
+			time.Sleep(5 * time.Second)
 
-		msg401 := s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			Expect().
-			Status(http.StatusUnauthorized).
-			Body().
-			Raw()
-		assert.Contains(ginkgo.GinkgoT(), msg401, "Missing authorization in request")
+			msg401 := s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				Expect().
+				Status(http.StatusUnauthorized).
+				Body().
+				Raw()
+			assert.Contains(ginkgo.GinkgoT(), msg401, "Missing authorization in request")
 
-		_ = s.NewAPISIXClient().GET("/ip").
-			WithHeader("Host", "httpbin.org").
-			WithHeader("Authorization", "Basic amFjazEtdXNlcm5hbWU6amFjazEtcGFzc3dvcmQ=").
-			Expect().
-			Status(http.StatusOK)
+			_ = s.NewAPISIXClient().GET("/ip").
+				WithHeader("Host", "httpbin.org").
+				WithHeader("Authorization", "Basic amFjazEtdXNlcm5hbWU6amFjazEtcGFzc3dvcmQ=").
+				Expect().
+				Status(http.StatusOK)
+		})
+	}
+
+	ginkgo.Describe("suite-annotations: scaffold v2beta3", func() {
+		suites(scaffold.NewDefaultScaffold)
+	})
+	ginkgo.FDescribe("suite-annotations: scaffold v2", func() {
+		suites(scaffold.NewDefaultV2Scaffold)
 	})
 })
